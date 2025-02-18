c82a1571a8d3c6132cdd50b4601e0b875911481a
Christian Rubiales <christianrubiales@yahoo.com>
2025-02-18 18:38:41 +0800

    log requester IP Address

--- src/main/java/com/christianrubiales/smchallenge/api/MailController.java
+++ src/main/java/com/christianrubiales/smchallenge/api/MailController.java
@@ -2,7 +2,10 @@ package com.christianrubiales.smchallenge.api;
 
 import com.christianrubiales.smchallenge.model.Mail;
 import com.christianrubiales.smchallenge.service.MailService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,20 +18,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MailController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MailController.class);
+
+    private final HttpServletRequest request;
+
     private final MailService mailService;
 
     @Autowired
-    public MailController(MailService mailService) {
+    public MailController(MailService mailService, HttpServletRequest request) {
         this.mailService = mailService;
+        this.request = request;
     }
 
     @PostMapping("/mail")
     public ResponseEntity<String> sendMail(@Valid @RequestBody Mail mail) {
+        logger.info("sendMail Request from {}: {}", this.getUserIP(this.request), mail);
 
         mailService.sendMail(mail);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body("Mail sent");
+    }
+
+    protected String getUserIP(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+        return ipAddress;
     }
 }
